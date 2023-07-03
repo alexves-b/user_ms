674e7e34d5c1e5786643f7ccce329bd2e3efef83
Алексей Веселов <alexves@bk.ru>
2023-07-03 19:50:29 +0400

    Create Controller

--- src/main/java/com/user/controller/AccountController.java
+++ src/main/java/com/user/controller/AccountController.java
@@ -1,6 +1,167 @@
 package com.user.controller;
 
+import com.user.dto.account.AccountDto;
+import com.user.dto.account.AccountSecureDto;
+import com.user.dto.account.AccountStatisticRequestDto;
+import com.user.dto.page.PageAccountDto;
+import com.user.dto.search.AccountSearchDto;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
 public class AccountController {
 
+    @Operation(summary = "get AccountByEmail", description = "Получение данных аккаунта по email", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account",
+            method = RequestMethod.GET)
+    ResponseEntity<AccountSecureDto> account() {
+        return new ResponseEntity<AccountSecureDto>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "Edit Account", description = "Обновление данных аккаунта", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account",
+            method = RequestMethod.PUT)
+    ResponseEntity<AccountDto> editAccount() {
+        return new ResponseEntity<AccountDto>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "create Account", description = "Создание аккаунта при регистрации", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account",
+            method = RequestMethod.POST)
+    ResponseEntity<AccountDto> createAccount() {
+        return new ResponseEntity<AccountDto>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "get account when login", description = "Получение своих данных при входе на сайт", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account/me",
+            method = RequestMethod.GET)
+    ResponseEntity<AccountDto> getAccountWhenLogin() {
+        return new ResponseEntity<AccountDto>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "edit account if login", description = "Обновление авторизованного аккаунта", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account/me",
+            method = RequestMethod.GET)
+    ResponseEntity<AccountDto> editAccountIfLogin() {
+        return new ResponseEntity<AccountDto>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "mark account for delete",
+            description = "Помечает авторизованный аккаунт как удалённый" +
+                    " и через заданное время стирает данные об аккаунте",
+            tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account/me",
+            method = RequestMethod.DELETE)
+    ResponseEntity<AccountSearchDto> markAccountForDelete() {
+        return new ResponseEntity<AccountSearchDto>(HttpStatus.OK);
+    }
+
+
+
+    @Operation(summary = "Send message about friends birthdays", description = "Отправляет друзьям сообщение о наступившем дне рождении",
+            tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account/birthdays",
+            method = RequestMethod.PUT)
+    ResponseEntity<String> sendMessageAboutFriendsBirthdays() {
+        return new ResponseEntity<String>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get account by id", description = "Получение данных по id", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account/{id}",
+            method = RequestMethod.GET)
+    ResponseEntity<AccountDto> getAccountById() {
+        return new ResponseEntity<AccountDto>(HttpStatus.OK);
+    }
+
+
+    @Operation(summary = "Delete account by id", description = "Полность удаляет аккаунт из базы по id", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account/{id}",
+            method = RequestMethod.DELETE)
+    ResponseEntity<String> deleteAccountById() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get all accounts, not work",
+            description = "Позволяет получить все аккаунты, не реализован", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account/unsupported",
+            method = RequestMethod.GET)
+    ResponseEntity<String> getAllAccounts() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+    @Operation(summary = "Get Statistic",
+            description = "Позволяет получить статистику по кол-ву" +
+                    " регистраций по возрастам и по кол-ву регистраций" +
+                    " по месяцам за указанный промежуток времени", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account/statistic",
+            method = RequestMethod.GET)
+    ResponseEntity<AccountStatisticRequestDto> getListAllAccounts() {
+        return new ResponseEntity<AccountStatisticRequestDto>(HttpStatus.OK);
+    }
+
+    @Operation(summary = "Get Account By statusCode",
+            description = "Позволяет получать аккаунты относительно запрашиваемого статуса", tags = {"Account service"})
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successful operation"),
+            @ApiResponse(responseCode = "400", description = "Bad request"),
+            @ApiResponse(responseCode = "401", description = "Unauthorized")})
+    @RequestMapping(value = "/api/v1/account/search/statusCode",
+            method = RequestMethod.GET)
+    ResponseEntity<PageAccountDto> getAccountByStatusCode() {
+        return new ResponseEntity<PageAccountDto>(HttpStatus.OK);
+    }
 
 }
+
