fdd7228f35ea1b1bf1bc715d3615eb2e0255e0f9
Алексей Веселов <alexves@bk.ru>
2023-07-18 20:01:03 +0400

    Ms now work on server

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,7 @@ FROM adoptopenjdk/openjdk11:ubi
 ARG JAR_FILE=target/usersMicroservice-0.0.1-SNAPSHOT.jar
 WORKDIR users
 COPY ${JAR_FILE} users_microservice.jar
+VOLUME liquibase/changelog/ d:/socialNetwork/users/src/main/resources/db/changelog/db.changelog-master.xml
+
 EXPOSE 8085
 CMD ["java","-jar", "users_microservice.jar"]
--- src/main/java/com/user/UserMicroserviceApplication.java
+++ src/main/java/com/user/UserMicroserviceApplication.java
@@ -3,7 +3,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
-@EnableAutoConfiguration
 public class UserMicroserviceApplication {
 
 	public static void main(String[] args) {
