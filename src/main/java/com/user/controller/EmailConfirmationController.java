4998e864bdaf18d62d82f0176b01fe75f7de66f0
Алексей Веселов <alexves@bk.ru>
2023-10-24 22:57:55 +0400

    Add endpoint recovery password and recovery password by secret question v99

--- src/main/java/com/user/controller/EmailConfirmationController.java
+++ src/main/java/com/user/controller/EmailConfirmationController.java
@@ -5,11 +5,16 @@ import com.user.service.impl.UserServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -19,51 +24,77 @@ import java.util.UUID;
 @Slf4j
 @Controller
 public class EmailConfirmationController {
-    private UUID uuidFromController;
-    private String email;
-    private String futureEmail;
-    private String presentEmail;
+
     private final UserServiceImpl userService;
+
     @Operation(summary = "return page for confirmation",
             description = "Подтверждение емейла при регистрации", tags = {"Account service"})
     @RequestMapping(value = "/api/v1/approve/{uuid}",
-            produces = MediaType.TEXT_HTML_VALUE,method = RequestMethod.GET)
-    public String PageConfirmationEmail(@PathVariable UUID uuid,Model model) {
+            produces = MediaType.TEXT_HTML_VALUE, method = RequestMethod.GET)
+    public String PageConfirmationEmail(@PathVariable UUID uuid, Model model, HttpServletResponse response) {
         log.warn(uuid.toString());
-        uuidFromController = uuid;
-        email = userService.getEmailByUUid(uuid);
-        Map <String,String> map = new HashMap<>();
-        map.put("email",email);
+        String email = userService.getEmailByUUid(uuid);
+        Cookie cookie = new Cookie("user_uuid", uuid.toString());
+        cookie.setHttpOnly(true);
+        cookie.setDomain("localhost");
+        cookie.setPath("/my_email/approve");
+        cookie.setMaxAge(24 * 60 * 60);
+        response.addCookie(cookie);
+        Map<String, String> map = new HashMap<>();
+        map.put("email", email);
         model.addAllAttributes(map);
         return "index";
     }
 
+
+
     @Operation(summary = "confirmation email",
             description = "Ваш емейл подтвержден, отправка страницы", tags = {"Account service"})
-    @RequestMapping(value = "/api/v1/approve/my_email", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE,
-            produces = MediaType.TEXT_HTML_VALUE,method = RequestMethod.POST)
+    @RequestMapping(value = "/api/v1/my_email/approve", consumes = MediaType.ALL_VALUE,
+            produces = MediaType.TEXT_HTML_VALUE, method = RequestMethod.POST)
     public String confirmationEmail(Model model,
                                     @RequestParam String answer,
-                                    @RequestParam Integer numberQuestion ) {
-        log.info(String.valueOf(uuidFromController));
-        Map <String,String> map = new HashMap<>();
-        map.put("email",email);
+                                    @RequestParam Integer numberQuestion,
+                                    HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        if(cookies !=null) {
+            for(Cookie c: cookies) {
+                System.out.println(c.getName() + " - " + c.getValue());
+            }
+        }
+        String userUuid = "7e4f8d1c-917d-4804-b2e4-a5bfef0d2967";
+        log.info(String.valueOf(userUuid));
+        Map<String, String> map = new HashMap<>();
+        String email = userService.getEmailByUUid(UUID.fromString(userUuid));
+        map.put("email", email);
         model.addAllAttributes(map);
-        userService.addRecoveryQuestionAndConfirmEmail(uuidFromController,numberQuestion,answer);
+        userService.addRecoveryQuestionAndConfirmEmail(UUID.fromString(userUuid), numberQuestion, answer);
         return "approved";
     }
+
+
+
 
     @Operation(summary = "return page for confirmation",
             description = "Страница подтверждерния изменения емейла", tags = {"Account service"})
     @RequestMapping(value = "api/v1/approve/change",
-            produces = MediaType.TEXT_HTML_VALUE,method = RequestMethod.GET)
-    public String pageChangeEmail(Model model, @RequestParam UUID uuid,@RequestParam String presentEmail, @RequestParam String futureEmail) {
-        Map <String,String> map = new HashMap<>();
+            produces = MediaType.TEXT_HTML_VALUE, method = RequestMethod.GET)
+    public String pageChangeEmail(Model model,
+                                  @RequestParam UUID uuid,
+                                  @RequestParam String presentEmail,
+                                  @RequestParam String futureEmail,
+                                  HttpServletResponse response) {
+
+        Cookie cookie = new Cookie("presentEmail", presentEmail);
+        cookie.setMaxAge(24 * 60 * 60); // expires in 1 day
+        Cookie cookie2 = new Cookie("futureEmail", futureEmail);
+        cookie2.setMaxAge(24 * 60 * 60);
+        response.addCookie(cookie);
+        response.addCookie(cookie2);
+        Map<String, String> map = new HashMap<>();
         log.info("viev page edit email");
-        this.futureEmail = futureEmail;
-        this.presentEmail = presentEmail;
-        map.put("email",presentEmail);
-        map.put("future_email",futureEmail);
+        map.put("email", presentEmail);
+        map.put("future_email", futureEmail);
         model.addAllAttributes(map);
         return "editEmail";
     }
@@ -72,52 +103,55 @@ public class EmailConfirmationController {
     @Operation(summary = "confirmation email",
             description = "Контроллер проверки кода", tags = {"Account service"})
     @RequestMapping(value = "/api/v1/code/approve/change_email_code", consumes = MediaType.ALL_VALUE,
-            produces = MediaType.TEXT_HTML_VALUE,method = RequestMethod.GET)
+            produces = MediaType.TEXT_HTML_VALUE, method = RequestMethod.GET)
     public String confirmationEditEmailByCode(Model model,
-                                    @RequestParam String code) {
+                                              @RequestParam String code,
+                                              @CookieValue(value = "presentEmail") String presentEmail,
+                                              @CookieValue(value = "futureEmail") String futureEmail) {
         int codeFromString;
         try {
             codeFromString = Integer.parseInt(code);
-        } catch (Exception exception){
+        } catch (Exception exception) {
             exception.printStackTrace();
             throw new ConfirmationCodeNotCorrect("Код подтверждения содержит только цифры");
         }
 
         log.info("код из контроллера: " + codeFromString + " емейл куда отправлен код: " + presentEmail);
-       if (!userService.checkConfirmationCode(codeFromString)) {
-          throw new ConfirmationCodeNotCorrect("Введен не верный код подтверждения");
-       } else {
-           userService.setEmail(presentEmail,futureEmail);
-           log.info("change email to: "+ futureEmail + " was confirmed by code from preveous email!" );
-           Map <String,String> map = new HashMap<>();
-           map.put("email",futureEmail);
-           model.addAllAttributes(map);
-           return "approved";
-       }
+
+        if (!userService.checkConfirmationCode(codeFromString)) {
+            throw new ConfirmationCodeNotCorrect("Введен не верный код подтверждения");
+        } else {
+            userService.setEmail(presentEmail, futureEmail);
+            log.info("change email to: " + futureEmail + " was confirmed by code from preveous email!");
+            Map<String, String> map = new HashMap<>();
+            map.put("email", futureEmail);
+            model.addAllAttributes(map);
+            return "approved";
+        }
     }
-
-
 
 
     @Operation(summary = "confirmation email",
             description = "Подтверждение емейла при регистрации", tags = {"Account service"})
     @RequestMapping(value = "/api/v1/code/approve/change_email", consumes = MediaType.ALL_VALUE,
-            produces = MediaType.TEXT_HTML_VALUE,method = RequestMethod.GET)
+            produces = MediaType.TEXT_HTML_VALUE, method = RequestMethod.GET)
     public String confirmationEditEmailByUUID(Model model,
-                                        @RequestParam String answer,
-                                        @RequestParam Integer numberQuestion ) {
+                                              @RequestParam String answer,
+                                              @RequestParam Integer numberQuestion,
+                                              @CookieValue(value = "presentEmail") String presentEmail,
+                                              @CookieValue(value = "futureEmail") String futureEmail) {
         log.info(answer);
         log.info(numberQuestion.toString());
         //сравнение контрольныйх впросов
 
-        if (!userService.checkRecoveryQuestionAndAnswer(presentEmail,answer,numberQuestion)) {
+        if (!userService.checkRecoveryQuestionAndAnswer(presentEmail, answer, numberQuestion)) {
             throw new ConfirmationCodeNotCorrect("Введен не верный ответ на контрольный вопрос" +
                     " или не правильно выбран контрольный вопрос!");
         } else {
-            userService.setEmail(presentEmail,futureEmail);
-            log.info("Change email to: "+ futureEmail + " was confirmed by recovery question!" );
-            Map <String,String> map = new HashMap<>();
-            map.put("email",futureEmail);
+            userService.setEmail(presentEmail, futureEmail);
+            log.info("Change email to: " + futureEmail + " was confirmed by recovery question!");
+            Map<String, String> map = new HashMap<>();
+            map.put("email", futureEmail);
             model.addAllAttributes(map);
             return "approved";
         }
