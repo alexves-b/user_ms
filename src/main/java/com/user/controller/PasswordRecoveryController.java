4a0d34f56b78b01a2fe570e368293c46d772edac
Алексей Веселов <alexves@bk.ru>
2023-10-26 17:32:39 +0400

    Refactor user_service v4

--- src/main/java/com/user/controller/PasswordRecoveryController.java
+++ src/main/java/com/user/controller/PasswordRecoveryController.java
@@ -24,20 +24,11 @@ public class PasswordRecoveryController {
 
     @Operation(summary = "recovery password",
             description = "Обработка запроса на отправку нового пароля на емейл", tags = {"Auth service"})
-    @RequestMapping(value = "/password/recovery/",
-            produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.POST)
-    public void sendNewPasswordToUser(@RequestBody RequestDtoChangeEmail dto) {
+    @RequestMapping(value = "/password/recovery/")
+    public String sendNewPasswordToUser(@RequestBody RequestDtoChangeEmail dto, Model model) {
         log.info("email for recovery: " + dto.getEmail().getEmail());
         userService.sendNewPasswordForUserEmail(dto.getEmail().getEmail());
-    }
-
-    @Operation(summary = "recovery password",
-            description = "Обработка запроса на отправку нового пароля на емейл", tags = {"Auth service"})
-    @RequestMapping(value = "/password/recovery/",
-            produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public String viev(@RequestBody RequestDtoChangeEmail dto) {
-       log.info("recovery sucsessful");
-       return "succsess";
+        return "success";
     }
 
     @Operation(summary = "view page for recovery password",
